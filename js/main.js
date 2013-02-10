7e34db1b9897e68fadd61ad6e9578e97c13bb2ee
eugeberzal <eugeberzal@gmail.com>
2013-02-09 23:55:14 -0300

    files

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Author: Eugenio Berzal --> http://www.eugenioberzal.com
+//Message display on the window.
+
+window.onbeforeunload = function() {
+    return 'ESTAS POR CERRAR LA VENTANA Y ES POSIBLE QUE PIERDAS LA SERIE/PELICULA DE CUEVANA! ESTAS SEGURO??';
+}
+
